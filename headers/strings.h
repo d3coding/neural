cd65a6acfb58febd0384f35748cb59a142803634
d3coding <mateusjuniordev@gmail.com>
2018-07-05 21:10:22 -0300

    Opps....

---
+++ headers/strings.h
@@ -0,0 +1,37 @@
+#pragma once
+
+//CLASS MATRIX
+
+#define TX0c00 "Criando matrix atraves de dados..."
+//
+#define TX0c02 "# # Adicionando weight..."
+#define TX0c03 "# # Adicionando bias..."
+
+#define ERR0c00 "# ERR # Error (Bias)"
+#define ERR0c01 "# ERR # Error (Weight)"
+#define ERR0c02 "# ERR # Error (Data)" 
+
+// CLASS TOOLS
+
+#define TX1c00 "# Treinando...\n"
+#define TX1c01 "# Sucesso ao treinar!...\n# Tempo decorrido: "
+#define TX1c02 "# Digite o valor de entrada para o neuronio("
+#define TX1c03 "# Mostrando resultados:\n"
+#define TX1c04 "Verdadeiro"
+#define TX1c05 "Falso"
+//
+//
+//
+#define TX1c10 "########## Weight ###########\n"
+#define TX1c11 "\n########## Bias ##########\n"
+#define TX1c12 "\n########## Experado ##########\n"
+#define TX1c13 "\n########## Erro ##########\n"
+#define TX1c14 "Total:\t\t"
+#define TX1c15 "Porcentagem:\t"
+#define TX1c16 "EXPER("
+#define TX1c17 "ERRO("
+//
+#define TX1c09 "\n# Aperte enter para continuar...\n"
+
+#define ERR1c00 "# ERR 1.00 # AS ITERACOES SAO 0!\n"
+#define ERR1c01 "# ERR 1.01 # A TAXA E 0!\n"
